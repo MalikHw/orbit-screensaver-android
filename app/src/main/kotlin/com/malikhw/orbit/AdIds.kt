7c1a645696e30e07dae6967115e7faf7b26cadc6
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-03-21 16:05:17 +0100

    Modify comment for AdIds object
    Updated comment to indicate IDs will be swapped later.

---
+++ app/src/main/kotlin/com/malikhw/orbit/AdIds.kt
@@ -0,0 +1,10 @@
+package com.malikhw.orbit.ads
+
+import com.malikhw.orbit.BuildConfig
+
+object AdIds {
+    // imma swap later
+    val BANNER_TOP    = "ca-app-pub-3940256099942544/6300978111"  // test
+    val BANNER_BOTTOM = "ca-app-pub-3940256099942544/6300978111"  // test
+    val INTERSTITIAL  = "ca-app-pub-3940256099942544/1033173712"  // test
+}
