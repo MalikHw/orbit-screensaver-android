e1ce8ca811b4729294baf5ac74151e0b6835ddfb
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-03-21 17:14:33 +0100

    Delete app/src/main/kotlin/com/malikhw/orbit/ads directory

--- app/src/main/kotlin/com/malikhw/orbit/ads/AdIds.kt
+++
@@ -1,9 +0,0 @@
-package com.malikhw.orbit.ads
-
-import com.malikhw.orbit.BuildConfig
-
-object AdIds {
-    val BANNER_TOP:    String get() = BuildConfig.ADMOB_BANNER_TOP
-    val BANNER_BOTTOM: String get() = BuildConfig.ADMOB_BANNER_BOTTOM
-    val INTERSTITIAL:  String get() = BuildConfig.ADMOB_INTERSTITIAL
-}
