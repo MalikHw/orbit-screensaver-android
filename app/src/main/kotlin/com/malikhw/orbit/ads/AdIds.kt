872be238117bef7a34304bb15b685724c8e59b14
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-03-21 16:45:00 +0100

    Refactor AdIds to use BuildConfig values

--- app/src/main/kotlin/com/malikhw/orbit/ads/AdIds.kt
+++ app/src/main/kotlin/com/malikhw/orbit/ads/AdIds.kt
@@ -3,8 +3,7 @@ package com.malikhw.orbit.ads
 import com.malikhw.orbit.BuildConfig
 
 object AdIds {
-    // Swappin this n***as later
-    val BANNER_TOP    = "ca-app-pub-3940256099942544/6300978111"  // test
-    val BANNER_BOTTOM = "ca-app-pub-3940256099942544/6300978111"  // test
-    val INTERSTITIAL  = "ca-app-pub-3940256099942544/1033173712"  // test
+    val BANNER_TOP:    String get() = BuildConfig.ADMOB_BANNER_TOP
+    val BANNER_BOTTOM: String get() = BuildConfig.ADMOB_BANNER_BOTTOM
+    val INTERSTITIAL:  String get() = BuildConfig.ADMOB_INTERSTITIAL
 }
