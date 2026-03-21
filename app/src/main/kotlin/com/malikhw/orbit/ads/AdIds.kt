72458a860e705490373e57d00ca64b143c430c9f
MalikHw47 <123307564+MalikHw@users.noreply.github.com>
2026-03-21 16:05:56 +0100

    Create AdIds.kt with ad unit IDs
    Added AdIds object with ad unit IDs for testing.

---
+++ app/src/main/kotlin/com/malikhw/orbit/ads/AdIds.kt
@@ -0,0 +1,10 @@
+package com.malikhw.orbit.ads
+
+import com.malikhw.orbit.BuildConfig
+
+object AdIds {
+    // Swappin this n***as later
+    val BANNER_TOP    = "ca-app-pub-3940256099942544/6300978111"  // test
+    val BANNER_BOTTOM = "ca-app-pub-3940256099942544/6300978111"  // test
+    val INTERSTITIAL  = "ca-app-pub-3940256099942544/1033173712"  // test
+}
